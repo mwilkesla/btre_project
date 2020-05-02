9009e90493ca0eb649ed692eafa0bb724f665d72
Matt <mwilkesla@gmail.com>
2020-05-02 17:03:51 -0500

    removed email from settings file

--- btre/settings.py
+++ btre/settings.py
@@ -142,12 +142,6 @@ MESSAGE_TAGS = {
     
 }
 
-# Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mwilkesla@gmail.com' #e.g. traversy.brad@gmail.com
-EMAIL_HOST_PASSWORD = 'welsharcher57#'
-EMAIL_USE_TLS = True
 
 try:
     from .local_settings import *
